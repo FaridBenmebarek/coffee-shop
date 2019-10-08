6a02661e4679f1d87b82e7c5e6e2d4a26f47dcce
Farid Benmebarek <farid.benmebarek@hotmail.fr>
2019-10-08 16:10:51 +0200

    add fix page 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,43 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import BackgroundSection from "./../components/Globals/BackgroundSection"
+import { graphql } from "gatsby"
+import Title from "./../components/Globals/Title"
 
-const NotFoundPage = () => (
+const NotFoundPage = ({ data }) => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="Erreur 404"
+      styleClass="about-background"
+    />
+    <section className="py-5">
+      <div className="container">
+        <Title title="Oops..." />
+        <div className="row">
+          <div className="col-10 col-sm-8 mx-auto text-center">
+            <p className="lead text-muted mb-5">
+              Le lien que vous avez saisi n'existe pas...
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   </Layout>
 )
 
 export default NotFoundPage
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "pageError.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
